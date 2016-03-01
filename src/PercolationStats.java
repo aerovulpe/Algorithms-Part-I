61dbe82926f279d34a15e87afbc3ff1357ed2848
Aaron Nwabuoku <aerisvulpe@gmail.com>
2016-03-01 15:14:45 -0500

    Fixed PercolationStat bug.

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -2,59 +2,67 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-	private double[] thresholds;
+    private double[] thresholds;
 
-	// perform T independent experiments on an N-by-N grid
-	public PercolationStats(int n, int t) {
-		if (n < 1 || t < 1)
-			throw new IllegalArgumentException("N <= 0 or T <= 0!");
+    /**
+     * perform t independent experiments on an n-by-n grid
+     *
+     * @param n
+     * @param t
+     */
+    public PercolationStats(int n, int t) {
+        if (n < 1 || t < 1)
+            throw new IllegalArgumentException("N <= 0 or T <= 0!");
 
-		thresholds = new double[t];
-		for (int c = 0; c < t; c++) {
-			final Percolation percolation = new Percolation(n);
-			int numOpened = 0;
-			while (!percolation.percolates()) {
-				int i = StdRandom.uniform(n) + 1;
-				int j = StdRandom.uniform(n) + 1;
 
-				if (!percolation.isOpen(i, j)) {
-					percolation.open(i, j);
-					numOpened++;
-				}
-			}
+        thresholds = new double[t];
+        for (int i = 0; i < t; i++) {
+            Percolation percolation = new Percolation(n);
+            int numOpened = 0;
+            double nSquared = n * n;
 
-			thresholds[c] = numOpened / n * n;
-		}
-	}
+            while (numOpened < nSquared) {
+                int p = StdRandom.uniform(1, n + 1);
+                int q = StdRandom.uniform(1, n + 1);
 
-	// sample mean of percolation threshold
-	public double mean() {
-		return StdStats.mean(thresholds);
-	}
+                if (!percolation.isOpen(p, q)) {
+                    numOpened++;
+                    percolation.open(p, q);
 
-	// sample standard deviation of percolation threshold
-	public double stddev() {
-		return StdStats.stddev(thresholds);
-	}
+                    if (percolation.percolates()) {
+                        thresholds[i] = numOpened / nSquared;
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
-	// low endpoint of 95% confidence interval
-	public double confidenceLo() {
-		return mean() - 1.96 * stddev() / Math.sqrt(thresholds.length);
-	}
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(thresholds);
+    }
 
-	// high endpoint of 95% confidence interval
-	public double confidenceHi() {
-		return mean() + 1.96 * stddev() / Math.sqrt(thresholds.length);
-	}
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(thresholds);
+    }
 
-	public static void main(String[] args) {
-		int n = Integer.parseInt(args[0]);
-		int t = Integer.parseInt(args[1]);
+    // low  endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return mean() - 1.96 * stddev() / Math.sqrt(thresholds.length);
+    }
 
-		PercolationStats percolationStats = new PercolationStats(n, t);
-		System.out.println("mean                    = " + percolationStats.mean());
-		System.out.println("stddev                  = " + percolationStats.stddev());
-		System.out.println("95% confidence interval = " + percolationStats.confidenceLo() + ", "
-				+ percolationStats.confidenceHi());
-	}
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return mean() + 1.96 * stddev() / Math.sqrt(thresholds.length);
+    }
+
+    public static void main(String[] args) {
+        PercolationStats percolationStats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        System.out.println("mean                    = " + percolationStats.mean());
+        System.out.println("stddev                  = " + percolationStats.stddev());
+        System.out.println("95% confidence interval = " + percolationStats.confidenceLo() + ", "
+                + percolationStats.confidenceHi());
+    }
 }
